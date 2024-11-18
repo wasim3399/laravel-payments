27235014c0031900bb7d7a969bb84dc88b88f4bf
Wasim Rashid <wasim.rashid@developers.studio>
2024-11-18 12:00:46 +0500

    implement telescope

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,4 +4,5 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\HorizonServiceProvider::class,
     App\Providers\RouteServiceProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
 ];
